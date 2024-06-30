fc5982efbdf9da9f4c7f5872ed635af4e449935e
HwangHyeonseok <51507287+HwangHyeonseok@users.noreply.github.com>
2024-06-30 21:07:07 +0900

    Title: 달에서 무게 구하기, Time: 107ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240630/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int weight = 13;
+        double rate = 0.165;
+        
+        System.out.printf("%d * %f = %.6f",weight, rate, weight*rate);
+    }
+}
