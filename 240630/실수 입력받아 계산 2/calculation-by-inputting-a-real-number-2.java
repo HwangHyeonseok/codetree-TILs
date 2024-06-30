c69ae53511307a4378d678c8b43bb9cd9e532321
HwangHyeonseok <51507287+HwangHyeonseok@users.noreply.github.com>
2024-06-30 21:44:58 +0900

    Title: 실수 입력받아 계산 2, Time: 159ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240630/실수 입력받아 계산 2/calculation-by-inputting-a-real-number-2.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a = Double.parseDouble(sc.nextLine());
+        System.out.printf("%.2f", a+1.5);
+    }
+}
