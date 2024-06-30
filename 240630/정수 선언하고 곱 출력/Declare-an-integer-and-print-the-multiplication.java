6621b828afbf20c674764d17e8a25fd24224200b
HwangHyeonseok <51507287+HwangHyeonseok@users.noreply.github.com>
2024-06-30 20:43:20 +0900

    Title: 정수 선언하고 곱 출력, Time: 97ms, Memory: 8MB, Status: Passed - CodeTree

--- 240630/정수 선언하고 곱 출력/Declare-an-integer-and-print-the-multiplication.java
+++ 240630/정수 선언하고 곱 출력/Declare-an-integer-and-print-the-multiplication.java
@@ -3,6 +3,6 @@ public class Main {
         int a = 26;
         int b = 5;
         int c = a*b;
-        System.out.pritnln(a + " * " + b + " = " + c);
+        System.out.println(a + " * " + b + " = " + c); // a*b는 자동으로 int 형으로 변환이 되지만 a+b는 String으로 인식된다.
     }
 }
