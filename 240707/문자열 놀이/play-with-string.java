df5c78b120f918bfd5acf8c28a2326a9e7d66005
HwangHyeonseok <51507287+HwangHyeonseok@users.noreply.github.com>
2024-07-07 21:22:03 +0900

    Title: 문자열 놀이, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240707/문자열 놀이/play-with-string.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+
+        String s = sc.next();
+        int q = sc.nextInt();
+
+        for(int i=0; i<q; i++) {
+            int command = sc.nextInt();
+
+            if(command == 1) {
+                int a = sc.nextInt();
+                int b = sc.nextInt();
+                char temp = s.charAt(a-1);
+                // a 인덱스와 b 인덱스가 바뀌어야 한다.
+                // aba 입력 시 1 2 입력되면, aab
+                s = s.substring(0, a-1) + s.charAt(b-1) + s.substring(a, b-1) + temp + s.substring(b);
+            }
+
+            else if(command == 2) {
+                String c = sc.next();
+                String d = sc.next();
+                s = s.replace(c,d);
+            }
+            System.out.println(s);
+        }
+    }
+}
