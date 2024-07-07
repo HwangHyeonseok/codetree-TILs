8d24de06ee9d46e9b1aad58ea34a7314030eae28
HwangHyeonseok <51507287+HwangHyeonseok@users.noreply.github.com>
2024-07-07 13:42:57 +0900

    Title: 부분문자열 위치 구하기, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240707/부분문자열 위치 구하기/find-location-of-substring.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        String s = sc.nextLine();
+        String sFind = sc.nextLine();
+
+        System.out.println(s.indexOf(sFind));
+
+    }
+}
