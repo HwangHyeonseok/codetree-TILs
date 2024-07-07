59197e1da9b7f9110a8a3d186676aa6f761240d0
HwangHyeonseok <51507287+HwangHyeonseok@users.noreply.github.com>
2024-07-07 21:31:40 +0900

    Title: 문자열 놀이, Time: 207ms, Memory: 11MB, Status: Passed - CodeTree

--- 240707/문자열 놀이/play-with-string.java
+++ 240707/문자열 놀이/play-with-string.java
@@ -1,7 +1,8 @@
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {        
+        Scanner sc = new Scanner(System.in);
         StringBuilder sb = new StringBuilder();
 
         String s = sc.next();
@@ -16,7 +17,15 @@ public class Main {
                 char temp = s.charAt(a-1);
                 // a 인덱스와 b 인덱스가 바뀌어야 한다.
                 // aba 입력 시 1 2 입력되면, aab
-                s = s.substring(0, a-1) + s.charAt(b-1) + s.substring(a, b-1) + temp + s.substring(b);
+                if(a>b) {
+                    temp = s.charAt(b-1);
+                    s = s.substring(0, b-1) + s.charAt(a-1) + s.substring(b, a-1) + temp + s.substring(a); 
+                }
+
+                else if(a<b) {
+                    s = s.substring(0, a-1) + s.charAt(b-1) + s.substring(a, b-1) + temp + s.substring(b);
+                }
+                
             }
 
             else if(command == 2) {
