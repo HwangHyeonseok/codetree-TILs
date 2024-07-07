232ee98f3b40a736a88e6e849be4378b061343bd
HwangHyeonseok <51507287+HwangHyeonseok@users.noreply.github.com>
2024-07-07 11:32:09 +0900

    Title: 문자열 길이의 합, Time: 119ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240707/문자열 길이의 합/sum-length-of-string.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(sc.nextLine());
+        for(int i=0; i<n; i++) {
+            String temp = sc.nextLine();
+            sb.append(temp);
+        }
+
+        int cnt = 0;
+        for(int i=0; i<sb.length(); i++) {
+            if(sb.charAt(i) == sb.charAt(0)) cnt++;
+        }
+
+        System.out.print(sb.length() + " " + cnt);
+
+    }
+}
